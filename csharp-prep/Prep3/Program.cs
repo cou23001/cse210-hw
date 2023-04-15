b64221867d2086fb416d75c585905f6cd7bd13f3
cou23001 <cou23001@byui.edu>
2023-04-15 00:42:26 -0700

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //int magic = int.Parse(Console.ReadLine());
+        
+        // Generate the random number
+        Random randomGenerator = new Random();
+        
+
+        int guesses = 0;
+
+        int guess;
+        string repeat = "Y";
+        
+        do
+        {
+            int magic = randomGenerator.Next(1, 5);
+
+            do 
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guesses++;
+                
+                if (guess < magic)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magic)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else{
+                    Console.WriteLine($"You guessed it! in {guesses} times");
+                    Console.Write("Keep playing (Y/N)? ");
+                    repeat = Console.ReadLine();
+                    guesses = 0;
+                }
+            } while (guess != magic);
+        } while (repeat != "N");
     }
 }
