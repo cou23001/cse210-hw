e18c5fa6530ca703d141ee92afa952e86fb71b4a
cou23001 <cou23001@byui.edu>
2023-04-15 14:14:05 -0700

    Prep 4 Stretch

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -8,6 +8,8 @@ class Program
         int total = 0;
         List<int> numbers;
         numbers = new List<int>();
+        List<int> positiveNumbers;
+        positiveNumbers = new List<int>();
         Console.WriteLine("Enter a list of numbers, type 0 when finished");
         
         do 
@@ -25,9 +27,22 @@ class Program
             total = total + number;
         }
 
+        foreach (int number in numbers) {
+            if (number > 0) {
+                positiveNumbers.Add(number);
+            }
+        }
+
         float average = ((float)total)/numbers.Count;
         Console.WriteLine($"The sum is:  {total}");
         Console.WriteLine($"The average is:  {average}");
         Console.WriteLine($"The largest number is:  {numbers.Max()}");
+        Console.WriteLine($"The smallest positive number is:  {positiveNumbers.Min()}");
+        Console.WriteLine($"The sorted list is:");
+        numbers.Sort();
+        foreach (int number in numbers) {
+            Console.WriteLine(number);
+        }
+
     }
 }
