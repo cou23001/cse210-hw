fe5b242d49cf2944928a97ac197411d99c6a5377
cou23001 <cou23001@byui.edu>
2023-04-14 23:21:08 -0700

    Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Prep 1
+
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine("");
+
+        Console.WriteLine($"Your name is {last}, {name} {last}.");
     }
 }
