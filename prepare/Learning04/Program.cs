19e61b8068f96389284f4ae0dd45fbffc31fba7f
cou23001 <cou23001@byui.edu>
2023-05-04 08:05:25 -0700

    Inheritance Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.WriteLine("Inheritance Learning Activity");
+        Assigment a = new Assigment("Samuel Bennett","Multiplication");
+        Console.WriteLine(a.GetSummary());
+
+        MathAssignment ma = new MathAssignment("Roberto Rodriguez","Fractions","7.3","8-19");
+        Console.WriteLine(ma.GetSummary());
+        Console.WriteLine(ma.GetHomeworkList());
+
+        WritingAssignment wa = new WritingAssignment("Mary Waters","European History","The Causes of World War II");
+        Console.WriteLine(wa.GetSummary());
+        Console.WriteLine(wa.GetWritingInformation());
     }
 }
