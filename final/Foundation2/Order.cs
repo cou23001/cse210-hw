fbcd64a36e948752cc106c4a87b3ee02030a6a07
cou23001 <cou23001@byui.edu>
2023-05-31 08:11:19 -0600

    Online Ordering update

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -1,30 +1,30 @@
 public class Order
 {
-     private List<Product> _products = new List<Product>();
-     private Customer _customer;
+    private List<Product> _products = new List<Product>();
+    private Customer _customer;
 
-     public Order (Customer customer)
-     {
+    public Order (Customer customer)
+    {
         _customer = customer;
-     }
+    }
 
     public void AddProduct(Product p)
     {
         _products.Add(p);
     }
-     public string GetTotalCostOrder()
-     {
+
+    public double GetTotalCostOrder()
+    {
         double total = 0;
         foreach (Product product in _products) {
             total = total + product.GetPriceProduct();
         }
         double value = total + GetShippingCost(_customer);
-        string formattedValue = $"{value:0.00}";
-        return formattedValue;
-     }
+        return Math.Round(value,2);
+    }
 
-     public double GetShippingCost(Customer customer)
-     {
+    public double GetShippingCost(Customer customer)
+    {
         if (customer.IsUSAResident())
         {
             return 5;
@@ -32,20 +32,20 @@ public class Order
         else{
             return 35;
         }
-     }
+    }
 
-     public string GetPackingLabel()
-     {
+    public string GetPackingLabel()
+    {
         string data = "\nPacking Label\n- ID - | Qty | Price | --- Product Name ---\n";
         foreach (Product p in _products)
         {
             data = data + $"{p.GetProductID()}     {p.GetQty()}      {p.GetPrice()}      {p.GetName()}\n";
         }
         return ($"{data}");
-     }
+    }
 
-     public string GetShippingLabel() 
-     {
+    public string GetShippingLabel() 
+    {
         return ($"{_customer.GetName()}\n{_customer.GetAddress()}");
-     }
+    }
 }
--- final/Foundation2/Product.cs
+++ final/Foundation2/Product.cs
@@ -1,39 +1,39 @@
 public class Product
 {
-    private string _name;
-    private string _productID;
-    private double _price;
-    private int _quantity;
+   private string _name;
+   private string _productID;
+   private double _price;
+   private int _quantity;
 
-     public Product(string n, string p_id, double p, int q)
-     {
+   public Product(string n, string p_id, double p, int q)
+   {
         _name = n;
         _productID = p_id;
         _price = p;
         _quantity = q;
-     }
+   }
 
-     public double GetPriceProduct()
-     {
+   public double GetPriceProduct()
+   {
         return _price * _quantity;
-     }
+   }
 
-     public string GetProductID()
-     {
-        return _productID;
-     }
+   public string GetProductID()
+   {
+      return _productID;
+   }
 
-     public string GetName()
-     {
-        return _name;
-     }
+   public string GetName()
+   {
+      return _name;
+   }
 
-     public int GetQty(){
-        return _quantity;
-     }
+   public int GetQty(){
+      return _quantity;
+   }
 
-     public double GetPrice()
-     {
-        return _price;
-     }
+   public double GetPrice()
+   {
+      return _price;
+   }
 }
